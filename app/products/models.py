d7531403c8efdac249438fe6963812215b332845
Jorique <guselnikovgeorgiy@gmail.com>
2024-10-02 14:13:57 +0300

    upd product models

---
+++ app/products/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import relationship, mapped_column, Mapped, relationships
+from app.database import Base
+
+
+class Products(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str]
+    price: Mapped[float] = mapped_column(nullable=False)
+    available_stock: Mapped[int] = mapped_column(nullable=False)
+
+    order_products: Mapped[list["Products"]] = relationship(back_populates="products")
