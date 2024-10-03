7d7c7afea040c825f1106270f7aa7a83e9311e95
Jorique <guselnikovgeorgiy@gmail.com>
2024-10-03 15:21:49 +0300

    added API exceptions

---
+++ app/exceptions.py
@@ -0,0 +1,24 @@
+from fastapi import HTTPException, status
+
+
+class APIException(HTTPException):
+    status_code = 500
+    detail = "Internal Server Error"
+
+    def __init__(self, status_code: int, detail: str):
+        super().__init__(status_code=status_code, detail=detail)
+
+
+class OutOfStockException(APIException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "Product out of stock"
+
+
+class NotEnoughStockException(APIException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "Requested quantity exceeds available stock"
+
+
+class NotFoundException(APIException):
+    status_code = status.HTTP_404_NOT_FOUND
+    detail = "Page not found"
