4d0ef498de13636c5dde42a5cb295a6ab184f650
Jorique <guselnikovgeorgiy@gmail.com>
2024-10-03 13:36:36 +0300

    pep

--- app/database.py
+++ app/database.py
@@ -2,9 +2,11 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from app.config import settings
 
+
 engine = create_async_engine(settings.DATABASE_URL)
 
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
 
 async def get_db_session():
     async with async_session_maker() as session:
