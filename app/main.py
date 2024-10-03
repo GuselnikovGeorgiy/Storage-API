3fdc08c173b8cee21c872f4d40dbadefb6f6b6fa
Jorique <guselnikovgeorgiy@gmail.com>
2024-10-03 19:16:15 +0300

    added routers to app

--- app/main.py
+++ app/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
+from app.orders.router import router as orders_router
+from app.products.router import router as products_router
 
 
 def create_app() -> FastAPI:
     app = FastAPI(
         title="Storage API",
-        root_path="/api/",
+        root_path="/api",
 
     )
+
+    app.include_router(orders_router)
+    app.include_router(products_router)
+
     return app
 
