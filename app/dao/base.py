6d73b7f125862f56fb0143790c04b4f3acef356f
Jorique <guselnikovgeorgiy@gmail.com>
2024-10-03 20:36:54 +0300

    moved methods to base dao

--- app/dao/base.py
+++ app/dao/base.py
@@ -1,8 +1,8 @@
-from sqlalchemy import insert, select
+from sqlalchemy import insert, select, update, delete
 from sqlalchemy.exc import SQLAlchemyError
 
 from app.database import async_session_maker
-from app.exceptions import SQLAlchemyException, APIException
+from app.exceptions import SQLAlchemyException, APIException, IDNotFoundException
 
 
 class BaseDAO:
@@ -42,3 +42,27 @@ class BaseDAO:
                 raise SQLAlchemyException
             elif isinstance(e, Exception):
                 raise APIException
+
+    @classmethod
+    async def update(cls, id: int, **data):
+        async with async_session_maker() as session:
+            query = select(cls.model.__table__.columns).filter_by(id=id)
+            result = await session.execute(query)
+            if result.mappings().one_or_none() is None:
+                raise IDNotFoundException
+
+            query = update(cls.model).where(cls.model.__table__.columns.id == id).values(**data)
+            await session.execute(query)
+            await session.commit()
+
+    @classmethod
+    async def delete(cls, id: int):
+        async with async_session_maker() as session:
+            query = select(cls.model.__table__.columns).filter_by(id=id)
+            result = await session.execute(query)
+            if result.mappings().one_or_none() is None:
+                raise IDNotFoundException
+
+            query = delete(cls.model).where(cls.model.__table__.columns.id == id)
+            await session.execute(query)
+            await session.commit()
--- app/products/dao.py
+++ app/products/dao.py
@@ -8,26 +8,3 @@ from app.exceptions import IDNotFoundException
 class ProductsDAO(BaseDAO):
     model = Products
 
-    @classmethod
-    async def update(cls, id: int, **data):
-        async with async_session_maker() as session:
-            query = select(cls.model.__table__.columns).filter_by(id=id)
-            result = await session.execute(query)
-            if result.mappings().one_or_none() is None:
-                raise IDNotFoundException
-
-            query = update(cls.model).where(cls.model.__table__.columns.id == id).values(**data)
-            await session.execute(query)
-            await session.commit()
-
-    @classmethod
-    async def delete(cls, id: int):
-        async with async_session_maker() as session:
-            query = select(cls.model.__table__.columns).filter_by(id=id)
-            result = await session.execute(query)
-            if result.mappings().one_or_none() is None:
-                raise IDNotFoundException
-
-            query = delete(cls.model).where(cls.model.__table__.columns.id == id)
-            await session.execute(query)
-            await session.commit()
