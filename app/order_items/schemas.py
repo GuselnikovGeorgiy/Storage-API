780d8b15a23026d84a0e52f1265fbf67e218b3cb
Jorique <guselnikovgeorgiy@gmail.com>
2024-10-03 20:41:26 +0300

    deleted order id

--- app/order_items/schemas.py
+++ app/order_items/schemas.py
@@ -2,6 +2,5 @@ from pydantic import BaseModel, Field
 
 
 class OrderItemsSchema(BaseModel):
-    order_id: int = Field(title="Идентификатор заказа")
     product_id: int = Field(title="Идентификатор продукта")
     quantity: int = Field(title="Количество товара в заказе", gt=0)
