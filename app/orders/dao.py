f1bbc9d4f5c6ab610e6ee4a71733ed74de6457b1
Jorique <guselnikovgeorgiy@gmail.com>
2024-10-04 17:41:45 +0300

    added session args, rmed local sessions

--- app/orders/dao.py
+++ app/orders/dao.py
@@ -1,8 +1,8 @@
 from app.dao.base import BaseDAO
-from app.database import async_session_maker
 from app.orders.models import Orders
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.encoders import jsonable_encoder
 
 
@@ -10,10 +10,9 @@ class OrdersDAO(BaseDAO):
     model = Orders
 
     @classmethod
-    async def select_joined(cls, **filter_by):
-        async with async_session_maker() as session:
-            query = select(Orders).filter_by(**filter_by).options(selectinload(Orders.order_items))
-            res = await session.execute(query)
-            res = res.scalars().all()
-            res = jsonable_encoder(res)
-            return res
+    async def select_joined(cls, session: AsyncSession, **filter_by):
+        query = select(Orders).filter_by(**filter_by).options(selectinload(Orders.order_items))
+        res = await session.execute(query)
+        res = res.scalars().all()
+        res = jsonable_encoder(res)
+        return res
