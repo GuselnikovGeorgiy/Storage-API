ede148a16162bf1017b827b6406ff7b918f45520
Jorique <guselnikovgeorgiy@gmail.com>
2024-10-04 17:42:51 +0300

    added date default

--- app/orders/models.py
+++ app/orders/models.py
@@ -1,5 +1,5 @@
 from datetime import date
-from sqlalchemy import Date
+from sqlalchemy import Date, func
 from sqlalchemy.orm import relationship, mapped_column, Mapped
 from app.database import Base
 
@@ -8,7 +8,7 @@ class Orders(Base):
     __tablename__ = "orders"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    created_at: Mapped[date] = mapped_column(Date, nullable=False)
+    created_at: Mapped[date] = mapped_column(Date, default=func.now())
     status: Mapped[str] = mapped_column(nullable=False)
 
     order_items: Mapped[list["OrderItems"]] = relationship("OrderItems", back_populates="order")
