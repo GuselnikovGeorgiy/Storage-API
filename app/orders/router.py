39f075a908eeb8a392aa87f718b8eb56d4b2e99b
Jorique <guselnikovgeorgiy@gmail.com>
2024-10-04 17:43:54 +0300

    rewrote cruds with session gen

--- app/orders/router.py
+++ app/orders/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from app.orders.dao import OrdersDAO
 from app.orders.schemas import OrderStatus, OrderSchema
+from app.orders.services import add_order
+from app.database import get_db_session
 
 
 router = APIRouter(
@@ -10,30 +12,45 @@ router = APIRouter(
 
 @router.post("/orders")
 async def create_order(order_data: OrderSchema):
-    pass
+    return await add_order(order_data)
 
 
 @router.get("/orders")
 async def get_orders():
-    return await OrdersDAO.select_all()
+    async with get_db_session() as session:
+        return await OrdersDAO.select_all(session=session)
 
 
 @router.get("/orders/items")
 async def get_orders_joined():
-    return await OrdersDAO.select_joined()
+    async with get_db_session() as session:
+        return await OrdersDAO.select_joined(session=session)
 
 
 @router.get("/orders/{order_id}")
 async def get_order(order_id: int):
-    return await OrdersDAO.find_one_or_none(id=order_id)
+    async with get_db_session() as session:
+        return await OrdersDAO.find_one_or_none(
+            session=session,
+            id=order_id
+        )
 
 
 @router.get("/orders/{order_id}/items")
 async def get_order_with_items(order_id: int):
-    return await OrdersDAO.select_joined(id=order_id)
+    async with get_db_session() as session:
+        return await OrdersDAO.select_joined(
+            session=session,
+            id=order_id
+        )
 
 
-@router.patch("/orders/{order_id}/{status}")
+@router.patch("/orders/{order_id}/status")
 async def update_order(order_id: int, status: OrderStatus):
-    return await OrdersDAO.update(id=order_id, status=status)
+    async with get_db_session() as session:
+        return await OrdersDAO.update(
+            session=session,
+            id=order_id,
+            status=status
+        )
 
