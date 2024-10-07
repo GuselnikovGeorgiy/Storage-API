e40cca4a0fc9d3e491c72d28bd6cbd363cb000b6
Jorique <guselnikovgeorgiy@gmail.com>
2024-10-08 01:10:18 +0300

    added  dublicate exc & added session arg

--- app/orders/services.py
+++ app/orders/services.py
@@ -3,54 +3,63 @@ from app.exceptions import (
     OutOfStockException,
     NotEnoughStockException,
     SQLAlchemyException,
+    UnprocessableEntityException,
 )
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
 from app.orders.schemas import OrderSchema
 from app.orders.dao import OrdersDAO
 from app.products.dao import ProductsDAO
 from app.order_items.dao import OrderItemsDAO
-from app.database import get_db_session
 
 
-async def add_order(order_data: OrderSchema):
+async def add_order(session: AsyncSession, order_data: OrderSchema):
     lst_products = []
+    products_ids = set()
     try:
-        async with get_db_session() as session:
-            for order_item in order_data.order_items:
-                product = await ProductsDAO.find_one_or_none(
-                    session=session,
-                    id=order_item.product_id,
-                )
-                if not product:
-                    raise IDNotFoundException
-                if product.available_stock == 0:
-                    raise OutOfStockException
-                if product.available_stock < order_item.quantity:
-                    raise NotEnoughStockException
+        if not order_data.order_items:
+            raise UnprocessableEntityException
 
-                lst_products.append(product)
+        for order_item in order_data.order_items:
+            if order_item.product_id in products_ids:
+                raise UnprocessableEntityException
+            products_ids.add(order_item.product_id)
 
-            for product, order_item in zip(lst_products, order_data.order_items):
-                await ProductsDAO.update(
-                    session=session,
-                    id=product.id,
-                    available_stock=product.available_stock - order_item.quantity
-                )
-
-            order_id_json = await OrdersDAO.add(
+        for order_item in order_data.order_items:
+            product = await ProductsDAO.find_one_or_none(
                 session=session,
-                status=order_data.status
+                id=order_item.product_id,
+            )
+            if not product:
+                raise IDNotFoundException
+            if product.available_stock == 0:
+                raise OutOfStockException
+            if product.available_stock < order_item.quantity:
+                raise NotEnoughStockException
+
+            lst_products.append(product)
+
+        for product, order_item in zip(lst_products, order_data.order_items):
+            await ProductsDAO.update(
+                session=session,
+                id=product.id,
+                available_stock=product.available_stock - order_item.quantity
             )
 
-            order_id = order_id_json['id']
+        order_id_json = await OrdersDAO.add(
+            session=session,
+            status=order_data.status
+        )
 
-            for order_item in order_data.order_items:
-                await OrderItemsDAO.add(
-                    session=session,
-                    order_id=order_id,
-                    product_id=order_item.product_id,
-                    quantity=order_item.quantity
-                )
+        order_id = order_id_json['id']
+
+        for order_item in order_data.order_items:
+            await OrderItemsDAO.add(
+                session=session,
+                order_id=order_id,
+                product_id=order_item.product_id,
+                quantity=order_item.quantity
+            )
     except (SQLAlchemyError, Exception) as e:
         if isinstance(e, SQLAlchemyError):
             raise SQLAlchemyException
