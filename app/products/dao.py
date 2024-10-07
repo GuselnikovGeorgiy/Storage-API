9a10be1635711719721433d42485035a3f93cde0
Jorique <guselnikovgeorgiy@gmail.com>
2024-10-08 01:08:18 +0300

    moved 'delete' from base dao

--- app/products/dao.py
+++ app/products/dao.py
@@ -1,10 +1,30 @@
 from app.dao.base import BaseDAO
+from app.order_items.models import OrderItems
 from app.products.models import Products
-from app.database import async_session_maker
-from sqlalchemy import select,update, delete
+from sqlalchemy import delete
 from app.exceptions import IDNotFoundException
+from sqlalchemy.exc import SQLAlchemyError
+from app.exceptions import SQLAlchemyException, APIException
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class ProductsDAO(BaseDAO):
     model = Products
 
+    @classmethod
+    async def delete(cls, session: AsyncSession, id: int):
+        item = await cls.find_one_or_none(session=session, id=id)
+        if not item:
+            raise IDNotFoundException
+
+        try:
+            query = delete(OrderItems).where(OrderItems.product_id == id)
+            await session.execute(query)
+
+            query = delete(cls.model).where(cls.model.__table__.columns.id == id)
+            await session.execute(query)
+        except (SQLAlchemyError, Exception) as e:
+            if isinstance(e, SQLAlchemyError):
+                raise SQLAlchemyException
+            elif isinstance(e, Exception):
+                raise APIException
