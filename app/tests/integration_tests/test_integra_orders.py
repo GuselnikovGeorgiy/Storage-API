464ca52e5f13d6cee1d32932573dc56aa2528c77
Jorique <guselnikovgeorgiy@gmail.com>
2024-10-08 21:19:58 +0300

    rmed test

--- app/tests/integration_tests/test_integra_orders.py
+++
@@ -1,8 +0,0 @@
-from httpx import AsyncClient
-
-
-async def test_get_orders(ac: AsyncClient):
-    response = await ac.get("/orders")
-
-    assert response.status_code == 200
-    assert len(response.json()) == 3
